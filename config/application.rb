e8e5cff67958ca719557473a860ad51eccff175d
Muntasim <muntasim@muntasim-laptop.(none)>
2011-04-16 17:48:41 +0600

    pagination link localized

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Travelobd
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :bn
 
     I18n.backend.class.send(:include, I18n::Backend::Fallbacks)
 
