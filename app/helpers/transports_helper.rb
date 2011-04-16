f75ea60d21f91950c2924ab7093e3606d50f4f13
Muntasim <muntasim@muntasim-laptop.(none)>
2011-04-16 16:39:30 +0600

    pagination link localized

--- app/helpers/transports_helper.rb
+++ app/helpers/transports_helper.rb
@@ -4,7 +4,7 @@ module TransportsHelper
     html = ''
     transport.departure_schedules.each do |schedule|
       html << "<div class='#{cycle('evenRow', 'oddRow')}'>"
-      html << "<div class='eventTitle'>#{schedule.route}</div>"
+      html << "<div class='eventTitle' style='margin-bottom: 0px'>#{schedule.route}</div>"
       html << "<div class=' departure_times'>#{schedule.time.split(',').collect { |item| '<span class="departure_time">'+ item + '</span>' }.join(' ')}</div>"
       html << "</div>"
     end
