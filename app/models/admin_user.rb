f8a1e058b2792865f54a2b3f27161d1e66ed9809
Muntasim <muntasim@muntasim-laptop.(none)>
2011-04-19 19:02:05 +0600

    for production server

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,21 +1,3 @@
-# == Schema Information
-#
-# Table name: admin_users
-#
-#  id               :integer(4)      not null, primary key
-#  first_name       :string(255)     default(""), not null
-#  last_name        :string(255)     default(""), not null
-#  role             :string(255)     not null
-#  email            :string(255)     not null
-#  status           :boolean(1)      default(FALSE)
-#  token            :string(255)     not null
-#  salt             :string(255)     not null
-#  crypted_password :string(255)     not null
-#  preferences      :string(255)
-#  created_at       :datetime
-#  updated_at       :datetime
-#
-
 class AdminUser < ActiveRecord::Base
 
   ROLE = Typus::Configuration.roles.keys.sort
