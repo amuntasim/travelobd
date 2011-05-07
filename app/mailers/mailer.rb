52a6a60ea3e157e2a06bc1402cf9eb0c6d980757
Muntasim <muntasim@muntasim-laptop.(none)>
2011-05-07 14:27:04 +0600

    ddd

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
 
   def password_reset_instructions(user)
     @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
-    mail(:to => "#{user.full_name} <#{user.email}>", :subject => "Password Reset Instructions", :from => "Equestrian Notifier <cst@everyequestrian.com>")
+    mail(:to => "#{user.full_name} <#{user.email}>", :subject => "Password Reset Instructions", :from => "Travelobd Notifier <cst@everyequestrian.com>")
   end
 
   def send_message(msg)
