9c4f10613bb04e5e08831ebb5e2f434cefe2dcf2
Muntasim <muntasim@muntasim-laptop.(none)>
2011-05-09 12:37:54 +0600

    ..

--- app/models/branch_location.rb
+++ app/models/branch_location.rb
@@ -2,4 +2,6 @@ class BranchLocation < ActiveRecord::Base
   belongs_to :branchable, :polymorphic => true
   belongs_to :district
   translates :location, :address, :phone
+
+  validates :district_id, :presence => true
 end
