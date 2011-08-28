da1dfb576cc93db2322e3771c77de8251abb8119
Muntasim <muntasim@muntasim.(none)>
2011-08-28 16:57:30 +0600

    -some issues fixed

---
+++ db/migrate/20110828095943_create_ownership_requests.rb
@@ -0,0 +1,20 @@
+class CreateOwnershipRequests < ActiveRecord::Migration
+  def self.up
+    create_table :ownership_requests do |t|
+      t.string :resource_type
+      t.integer :resource_id
+      t.string :name
+      t.string :company
+      t.string :designation
+      t.string :phone
+      t.string :email
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :ownership_requests
+  end
+end
